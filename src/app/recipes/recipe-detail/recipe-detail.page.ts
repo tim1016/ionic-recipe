80453f958ab2a0d9f000f6cecf394978cd8e5483
Inkant Awasthi <10158214+tim1016@users.noreply.github.com>
2020-03-14 23:19:53 -0500

    developing recipe services

--- src/app/recipes/recipe-detail/recipe-detail.page.ts
+++ src/app/recipes/recipe-detail/recipe-detail.page.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { RecipesService } from '../recipes.service';
+import { Recipe } from '../recipes.model';
 
 @Component({
   selector: 'app-recipe-detail',
   templateUrl: './recipe-detail.page.html',
-  styleUrls: ['./recipe-detail.page.scss'],
+  styleUrls: ['./recipe-detail.page.scss']
 })
 export class RecipeDetailPage implements OnInit {
-
-  constructor() { }
+  loadedRecipe: Recipe;
+  constructor(private activatedRoute: ActivatedRoute, private recipeService: RecipesService, private router: Router) {}
 
   ngOnInit() {
+    this.activatedRoute.paramMap.subscribe(paramMap => {
+      if (!paramMap.has('recipeId')) {
+        return;
+      }
+      const recipeId = paramMap.get('recipeId');
+      this.loadedRecipe = this.recipeService.getRecipe(recipeId);
+    });
   }
 
+  onDeleteRecipe = () => {
+    this.recipeService.deleteRecipe(this.loadedRecipe.id);
+    this.router.navigate(['/recipes']);
+  };
 }
--- src/app/recipes/recipes.page.ts
+++ src/app/recipes/recipes.page.ts
@@ -1,27 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
 import { Recipe } from './recipes.model';
+import { RecipesService } from './recipes.service';
 
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.page.html',
   styleUrls: ['./recipes.page.scss']
 })
-export class RecipesPage implements OnInit {
-  recipes: Recipe[] = [
-    {
-      id: 'r1',
-      title: 'Schnitzel',
-      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
-      ingredients: ['French Fries', 'Pork Meat', 'Salad']
-    },
-    {
-      id: 'r2',
-      title: 'Spaghetti',
-      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/0/05/Classic-spaghetti-carbonara.jpg',
-      ingredients: ['Tomato sauce', 'Garlic', 'Basil']
-    }
-  ];
-  constructor() {}
+export class RecipesPage implements OnInit, OnChanges {
+  recipes: Recipe[];
+  constructor(private recipeService: RecipesService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.recipes = this.recipeService.getAllRecipes();
+    console.log(this.recipes);
+  }
+
+  ngOnChanges = () => {
+    this.recipes = this.recipeService.getAllRecipes();
+  };
 }
